d18a4ae4a19b96af252b9dcb77fa7c7d7d15d59a
Jed Laundry <jlaundry@jlaundry.com>
2022-02-01 14:09:08 +1300

    fixed break/continue error

--- twitter/__init__.py
+++ twitter/__init__.py
@@ -6,6 +6,8 @@ import os
 import requests
 
 import azure.functions as func
+from azure.data.tables import TableServiceClient, UpdateMode
+from azure.core.credentials import AzureSasCredential
 
 # https://birdie0.github.io/discord-webhooks-guide/discord_webhook.html
 
@@ -23,11 +25,13 @@ def tweets_to_webhooks(tweets:dict):
 
         # Resolve original RT
         if "referenced_tweets" in tweet.keys():
+            logging.debug(f"Looking at referenced_tweets:{tweet['referenced_tweets']}")
             for included_tweet in tweets['includes']['tweets']:
-                print(included_tweet)
+                logging.debug(f"  included tweet:{included_tweet}")
                 if included_tweet['id'] == tweet['referenced_tweets'][0]['id']:
+                    logging.debug("found!")
                     tweet = included_tweet
-                    continue
+                    break
 
         created_at = datetime.strptime(tweet['created_at'], "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
 
@@ -40,7 +44,7 @@ def tweets_to_webhooks(tweets:dict):
                     "url": f"https://www.twitter.com/{user['username']}",
                     "icon_url": user['profile_image_url']
                 },
-                "title": f"Tweet {tweet['id']}",
+                "title": f"Tweet from {user['username']}",
                 "url": f"https://twitter.com/{user['username']}/status/{tweet['id']}",
                 "description": tweet['text'],
                 "color": 1940464,
@@ -60,28 +64,26 @@ def tweets_to_webhooks(tweets:dict):
             }]
         }
 
-        print(json.dumps(data, indent=4))
+        logging.debug(json.dumps(data, indent=4))
 
         requests.post(os.environ['DiscordWebhook'], json=data)
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.utcnow().replace(
-        tzinfo=timezone.utc).isoformat()
+    utc_timestamp = datetime.utcnow().replace(tzinfo=timezone.utc).isoformat()
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
-if __name__ == '__main__':
-    # tweets = json.load(open('example.json', 'r'))
+    table_service_client = TableServiceClient.from_connection_string(os.environ['TableStorage'])
+    table_client = table_service_client.get_table_client(table_name="twitterbot")
 
-    users = [
-        (106554518, 1487266946928328704),
-    ]
+    for entity in  table_client.list_entities():
+        user_id = entity['RowKey']
+        since_id = entity['since_id']
 
-    for (user_id, since_id) in users:
         tweets = requests.get(
             f"https://api.twitter.com/2/users/{user_id}/tweets",
             params={
@@ -96,6 +98,14 @@ if __name__ == '__main__':
             },
         )
 
-        print((json.dumps(tweets.json(), indent=2)))
+        tweets_json = tweets.json()
+        logging.debug((json.dumps(tweets_json, indent=2)))
 
-        tweets_to_webhooks(tweets.json())
+        if tweets_json['meta']['result_count'] == 0:
+            continue
+        else:
+            tweets_to_webhooks(tweets_json)
+            entity['since_id'] = tweets_json['meta']['newest_id']
+            table_client.update_entity(mode=UpdateMode.REPLACE, entity=entity)
+
+    logging.info('Function complete at %s', datetime.utcnow().isoformat())
